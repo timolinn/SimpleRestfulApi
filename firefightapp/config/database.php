6020a0b496e4df32a9c1de322bed5b832423036a
Timmy <fabrobocomx@gmail.com>
2017-04-30 18:23:51 +0100

    Cleardb

--- firefightapp/config/database.php
+++ firefightapp/config/database.php
@@ -30,8 +30,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-    /:@/heroku_c75e00f58645d47?reconnect=true
-
+  
     'connections' => [
 
         'sqlite' => [
